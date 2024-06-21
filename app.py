aaccf97b866e381bba6609f03afdd7ce7da6d2b1
Vijay Kethanaboyina <vijaykethanaboyina@gmail.com>
2024-06-20 23:42:41 -0700

    added support for laoding pregenerated files. next task is to generate dataset in gui_free_generation.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import re
-from random import choice
+from random import choice, randint
 from backend import evaluate_translation, get_wrong_words, get_sentence_starter, run_search, gpt_rank, identify_cognates, init_beam_search, make_sentence_object
+import json
 
 app = Flask(__name__, static_folder="templates/static")
+USE_PREGENERATED_DATA = True
 
 # it's helpful to not hard-code the lang codes
 lang_codes = {
@@ -15,6 +17,10 @@ lang_codes = {
 src_lang = 'en'
 target_lang = 'fr'
 candidates = []
+
+# read production_data/ui_mockup.json
+with open('production_data/preloaded.json', 'r') as f:
+    stories = json.load(f)['stories']
 
 def get_highlighted(sentence, cognate_list):
     if (sentence == None or sentence == ""):
@@ -67,25 +73,59 @@ def one_step_forward(curr_sentence):
 
     output = {
       'sentence': highlighted_sentence,
-      'score': best_sentence_object.score_breakdown['total_score']
+      'score': best_sentence_object.score_breakdown['total_score'],
+      "is_pregenerated": False
     }
     return output
 
 @app.route('/generate_sentence', methods=['POST'])
 def generate_sentence():
-    sentence_starter = get_sentence_starter()
-    sentence_object = make_sentence_object(sentence_starter)
+    if USE_PREGENERATED_DATA:
+      index = randint(0, len(stories) - 1)
+      sentence_list = stories[index]['sentences']
+      sentence = sentence_list[0]['raw_text']
+      score = sentence_list[0]['score']
+      output = {
+        'sentence': sentence,
+        'score': score,
+        'is_pregenerated': True,
+        "story_index": index,
+        "sentence_index": 0
+      }
+      return jsonify(output)
+    else:
+      sentence_starter = get_sentence_starter()
+      sentence_object = make_sentence_object(sentence_starter)
 
-    beams = init_beam_search(sentence_object, 3)
-    return jsonify(one_step_forward(beams))
+      beams = init_beam_search(sentence_object, 3)
+      return jsonify(one_step_forward(beams))
 
 @app.route('/extend_sentence', methods=['POST'])
 def extend_sentence():
     data = request.get_json()
-    sentence_object = make_sentence_object(data['original_sentence'])
+    if ('story_index' in data.keys()):
 
-    beams = init_beam_search(sentence_object, 3)
-    return jsonify(one_step_forward(beams))
+      story_index = int(data['story_index'])
+      sentence_index = (int(data['sentence_index']) + 1 ) % len(stories[story_index]['sentences'])
+
+      sentence_list = stories[story_index]['sentences']
+      sentence = sentence_list[sentence_index]['raw_text']
+      score = sentence_list[sentence_index]['score']
+
+      output = {
+        'sentence': sentence,
+        'score': score,
+        'is_pregenerated': True,
+        "story_index": story_index,
+        "sentence_index": sentence_index
+      }
+      return jsonify(output)
+    else:
+      data = request.get_json()
+      sentence_object = make_sentence_object(data['original_sentence'])
+
+      beams = init_beam_search(sentence_object, 3)
+      return jsonify(one_step_forward(beams))
 
 if __name__ == '__main__':
     app.run(debug=True)
